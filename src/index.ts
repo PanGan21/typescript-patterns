6a99a0423dc1393b16a4bae38b823737aeffd76a
PanGan21 <panagiotisganelis@Panagiotiss-MacBook-Pro.local>
2020-09-14 00:25:05 +0200

    load google api map

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import { Company } from "./Company";
 import { User } from "./User";
 
+// Load google api in html
+const API_KEY = process.env.GOOGLEMAP_API_KEY;
+const script = document.createElement("script");
+script.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
+document.body.append(script);
+
 const user = new User();
 const company = new Company();
 console.log(user);
